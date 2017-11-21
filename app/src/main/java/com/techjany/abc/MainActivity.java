c837e888eeb70755206a2cfbbbf997424b49939b
Md. Azizul Haque Jany <shawpnochari13@gmail.com>
2017-11-21 14:14:57 +0600

    add login page & shared preference

--- app/src/main/java/com/techjany/abc/MainActivity.java
+++ app/src/main/java/com/techjany/abc/MainActivity.java
@@ -1,39 +1,102 @@
 package com.techjany.abc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher,
+        CompoundButton.OnCheckedChangeListener{
+    private EditText etUsername, etPass;
+    private Button btnLogin;
+    private CheckBox rem_userpass;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    private static final String PREF_NAME = "prefs";
+    private static final String KEY_REMEMBER = "remember";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASS = "password";
 
-    EditText emailET;
-    EditText passET;
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        emailET = (EditText) findViewById(R.id.emailET);
-        passET = (EditText) findViewById(R.id.passET);
-        button = (Button) findViewById(R.id.button);
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        etUsername = (EditText)findViewById(R.id.username);
+        etPass = (EditText)findViewById(R.id.pass);
+        rem_userpass = (CheckBox)findViewById(R.id.checkBox);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        if(sharedPreferences.getBoolean(KEY_REMEMBER, false))
+            rem_userpass.setChecked(true);
+        else
+            rem_userpass.setChecked(false);
+
+        etUsername.setText(sharedPreferences.getString(KEY_USERNAME,""));
+        etPass.setText(sharedPreferences.getString(KEY_PASS,""));
+
+        etUsername.addTextChangedListener(this);
+        etPass.addTextChangedListener(this);
+        rem_userpass.setOnCheckedChangeListener(this);
+
 
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        managePrefs();
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        managePrefs();
+    }
+
+    private void managePrefs(){
+        if(rem_userpass.isChecked()){
+            editor.putString(KEY_USERNAME, etUsername.getText().toString().trim());
+            editor.putString(KEY_PASS, etPass.getText().toString().trim());
+            editor.putBoolean(KEY_REMEMBER, true);
+            editor.apply();
+        }else{
+            editor.putBoolean(KEY_REMEMBER, false);
+            editor.remove(KEY_PASS);
+            editor.remove(KEY_USERNAME);
+            editor.apply();
+        }
+    }
 
     public void buttonClick(View view) {
-        String email = emailET.getText().toString();
-        String pass = passET.getText().toString();
+        String email = etUsername.getText().toString();
+        String pass = etPass.getText().toString();
 
         String oemail = "admin";
         String opass = "admin";
 
         if (email.equals(oemail) && pass.equals(opass)){
+
 
             Intent intent = new Intent(this, Navigation.class);
             startActivity(intent);
@@ -42,5 +105,4 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Invalid mail/Password", Toast.LENGTH_SHORT).show();
         }
     }
-
 }
